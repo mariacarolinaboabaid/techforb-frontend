2080d36f4eeb2ef5b553e03513fc01b8bd5961fb
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-09-06 10:34:25 -0300

    feautre [criação do navbar]

--- app-unicomer/src/app/app.module.ts
+++ app-unicomer/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { PublicModule } from './shared/layouts/public/public.module';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
     
   ],
   imports: [
